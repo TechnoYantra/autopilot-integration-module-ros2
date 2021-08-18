1c7f3f4b389973ddc66b0d80731246b1c4590261
Chirag Makwana <chiragmakwana02@gmail.com>
2021-08-19 00:36:04 +0530

    port setpoint

--- ty_autopilot_core/ty_autopilot_core/node/setpoint.py
+++ ty_autopilot_core/ty_autopilot_core/node/setpoint.py
@@ -1,47 +1,37 @@
 from ty_autopilot_core.base_node  import BaseNode
-
+import rclpy
 import threading
 import time
 import mavros
-
+import rclpy.qos
 from math import *
-from ty_autopilot_core.mavutils import setpoint as SP
 from tf_transformations import quaternion_from_euler
-from geometry_msgs.msg import PoseStamped, Twist
+from geometry_msgs.msg import PoseStamped, Twist, Quaternion
+from std_msgs.msg import Header
 sensor_qos = rclpy.qos.qos_profile_sensor_data
 
 
-class SetpointPosition:
+class SetpointPosition(BaseNode):
     def __init__(self):
         self.x = 0.0
         self.y = 0.0
         self.z = 0.0
         
         # publisher for mavros/setpoint_position/local
-        # self.pub = rospy.Publisher("mavros/setpoint_position/local", PoseStamped, queue_size=10) #SP.get_pub_position_local(queue_size=10)        
         self.pub = BaseNode.Publisher(self, PoseStamped, "/mavros/setpoint_position/local", sensor_qos)
 
         # subscriber for mavros/local_position/local
-        # self.sub = rospy.Subscriber("mavros/local_position/pose", PoseStamped, self.reached)
-        BaseNode.subscribe_topic(self,"/mavros/local_position/pose", PoseStamped, sensor_qos)
-        # try:
-        #     thread.start_new_thread(self.navigate, ())
-        # except:
-        #     pass
+        self.sub = BaseNode.subscribe_topic(self, "/mavros/local_position/pose",PoseStamped, sensor_qos)
 
         self.done = False
         self.done_evt = threading.Event()
 
     def navigate(self, x, y, z):
-        rate = rospy.Rate(10) 
-
         msg = PoseStamped(
-            header=SP.Header(
+            header=Header(
                 frame_id="base_link",  
-                stamp=rospy.Time.now()),   
+                stamp=self.get_clock().now().to_msg()),   
         )
-
-        # while not rospy.is_shutdown() or not self.done:
         
         msg.pose.position.x = x
         msg.pose.position.y = y
@@ -50,102 +40,66 @@ class SetpointPosition:
         yaw_degrees = 0 
         yaw = radians(yaw_degrees)
         quaternion = quaternion_from_euler(0, 0, yaw)
-        msg.pose.orientation = SP.Quaternion(*quaternion)
+        msg.pose.orientation = Quaternion(*quaternion)
         if not self.done:
             self.pub.publish(msg)
-            rate.sleep()
+            time.sleep(0.1)
 
     def set(self, x, y, z, delay=0, wait=True):
         self.done = False
         self.x = x
         self.y = y
         self.z = z
-        print("setting")
-        if wait:
-            rate = rospy.Rate(5)
-            while not self.done and not rospy.is_shutdown():
-                # print("sp loop")
-                rate.sleep()
-        time.sleep(delay)
-        self.navigate(x,y,z)
-
+        time.sleep(0.1)
+        msg = PoseStamped(
+            header=Header(
+                frame_id="base_link",  
+                stamp=self.get_clock().now().to_msg()),   
+        )        
+        msg.pose.position.x = x
+        msg.pose.position.y = y
+        msg.pose.position.z = z
+        yaw_degrees = 0 
+        yaw = radians(yaw_degrees)
+        quaternion = quaternion_from_euler(0, 0, yaw)
+        self.get_logger().info(str(quaternion[0]))
+        msg.pose.orientation.x = quaternion[0] 
+        msg.pose.orientation.y = quaternion[1] 
+        msg.pose.orientation.z = quaternion[2] 
+        msg.pose.orientation.w = quaternion[3] 
+        if not self.done:
+            self.pub.publish(msg)
+            time.sleep(0.1)
+        pose = BaseNode.read_topic(self, '/mavros/local_position/pose')
+        def is_near(msg, x, y):
+            return abs(x - y) < 1.5
+        if is_near('X', pose.pose.position.x, self.x) and is_near('Y', pose.pose.position.y, self.y):
+            self.done = True
+            self.done_evt.set()
+        else:
+            self.done = False
         if self.done:
-            # print("True")
             return True
 
 
-    def reached(self, topic):
-        def is_near(msg, x, y):
-            #rospy.loginfo("Position %s: local: %d, target: %d, abs diff: %d", msg, x, y, abs(x - y))
-            return abs(x - y) < 1.5
-
-        if is_near('X', topic.pose.position.x, self.x) and \
-           is_near('Y', topic.pose.position.y, self.y):
-            self.done = True
-            self.done_evt.set()
-
-class SetpointVelocity:
+class SetpointVelocity(BaseNode):
     def __init__(self):
-        self.velocity_pub = rospy.Publisher("sp_vel/cmd_vel", Twist , queue_size=5)
-        thread.start_new_thread(self.navigate, ())
+        self.velocity_pub = BaseNode.Publisher(self, Twist,"sp_vel/cmd_vel", 10)
 
-        self.twist = Twist()
         self.vx = 0.0
         self.vy = 0.0
         self.vz = 0.0
         self.yaw_rate = 0.0
 
     def set(self,vx,vy,vz,yaw_rate):
-        self.vx = vx
-        self.vy = vy
-        self.vz = vz
-        self.yaw_rate = yaw_rate
-    
-    def navigate(self):
-        rate = rospy.Rate(10) 
-        while not rospy.is_shutdown():
-            self.twist.linear.x = self.vx
-            self.twist.linear.y = self.vy
-            self.twist.linear.z = self.vz
+        twist = Twist()
+        twist.linear.x = vx
+        twist.linear.y = vy
+        twist.linear.z = vz
 
-            self.twist.angular.x = 0.0
-            self.twist.angular.y = 0.0
-            self.twist.angular.z = self.yaw_rate
-            self.velocity_pub.publish(self.twist)
-            rate.sleep()
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
+        twist.angular.z = yaw_rate
 
-
-
-
-
-
-# def rover_test():
-#     rospy.init_node('setpoint_position_demo')
-#     rate = rospy.Rate(10)
-
-#     setpoint = SetpointPosition()
-
-#     rospy.loginfo("driving to the right")
-#     setpoint.set(5.0, 0.0, 0.0, 5)
-
-#     rospy.loginfo("driving to the left")
-#     setpoint.set(0.0, -5.0, 0.0, 5)
-
-#     rospy.loginfo("driving to the right")
-#     setpoint.set(-5.0, -5.0, 0.0, 5)
-
-#     rospy.loginfo("driving to the left")
-#     setpoint.set(0.0, 5.0, 0.0, 5)
-
-#     offset_x = 0.0
-#     offset_y = 0.0
-#     offset_z = 0.0
-#     sides = 360
-#     radius = 20
-
-
-# if __name__ == '__main__':
-#     try:
-#         rover_test()
-#     except rospy.ROSInterruptException:
-#         pass
+        self.velocity_pub.publish(twist)
+        time.sleep(0.1)
