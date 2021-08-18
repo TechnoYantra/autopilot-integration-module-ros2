cac6d662f53bd3fac822fefcc2cdc49f6e287996
Chirag Makwana <chiragmakwana02@gmail.com>
2021-08-19 00:33:46 +0530

    port rc_override

--- ty_autopilot_core/ty_autopilot_core/node/rc_override.py
+++ ty_autopilot_core/ty_autopilot_core/node/rc_override.py
@@ -1,36 +1,27 @@
-#! /usr/bin/python
-
-import rospy
 import sys
 import time
+from ty_autopilot_core.mavutils.param_util import RosParam
+from ty_autopilot_core.base_node import BaseNode
 
 from mavros_msgs.msg import OverrideRCIn
 from ty_autopilot_core.mavutils.mavenum import RcOption
 
-class MavrosRC():
-
+class MavrosRC(BaseNode):
     def __init__(self):
-        rospy.init_node("mavros_rc_override_node")
-        self.rc_pub = rospy.Publisher("/mavros/rc/override", OverrideRCIn, queue_size=1)
-        self.rate = rospy.Rate(1)
+        BaseNode.get_logger(self).info("Starting mavros_rc_override")
+        self.rc_pub = BaseNode.Publisher(self,OverrideRCIn, "/mavros/rc/override", 10)
 
-    def publish(self):
-        while not rospy.is_shutdown():
-            channels=[0]*8
-            rc_options = rospy.get_param('rc_options', channels)
-            disable_gps = rospy.get_param('disable_gps', False)
-            pose_source = rospy.get_param('mav_pose_source',1)
-            msg = OverrideRCIn()
-            msg.channels = channels 
-            if RcOption.EKF_POS_SOURCE.value in rc_options:
-                msg.channels[rc_options.index(RcOption.EKF_POS_SOURCE.value)] = 1000+(500*(pose_source-1))
-            if RcOption.GPS_DISABLE.value in rc_options:
-                msg.channels[rc_options.index(RcOption.GPS_DISABLE.value)] = 1000+(1000*int(disable_gps))
-            print(msg)
-            self.rc_pub.publish(msg)
-            rospy.sleep(0.5)
+    def publish_rc_channels(self,channels = [0]*8):
+        BaseNode.get_logger(self).info(str(channels))
+        rc_options = channels
+        disable_gps = RosParam.get_param(self, 'disable_gps', False)
+        pose_source = RosParam.get_param(self, 'mav_pose_source',1)
+        msg = OverrideRCIn()
+        msg.channels = channels 
+        if RcOption.EKF_POS_SOURCE.value in rc_options:
+            msg.channels[rc_options.index(RcOption.EKF_POS_SOURCE.value)] = 1000+(500*(pose_source-1))
+        if RcOption.GPS_DISABLE.value in rc_options:
+            msg.channels[rc_options.index(RcOption.GPS_DISABLE.value)] = 1000+(1000*int(disable_gps))
+        self.rc_pub.publish(msg)
 
-if __name__ == '__main__':
-    mavros_rc = MavrosRC()
-    mavros_rc.publish()
 
