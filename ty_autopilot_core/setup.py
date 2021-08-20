7069dd33430d8cdd283e376e7e96e4ba363d3cd2
Chirag Makwana <chiragmakwana02@gmail.com>
2021-08-21 00:36:10 +0530

    update setup.py

--- ty_autopilot_core/setup.py
+++ ty_autopilot_core/setup.py
@@ -13,7 +13,6 @@ setup(
     packages=[package_name, mavutils, node, services],
     data_files=[('share/ament_index/resource_index/packages',['resource/' + package_name]),
                 ('share/' + package_name, ['package.xml']),
-                (os.path.join('share', package_name), glob('launch/*.xml')),
                 (os.path.join('share', package_name), glob('launch/*.py')),
                 (os.path.join('share', package_name), glob('config/*'))
     ],
